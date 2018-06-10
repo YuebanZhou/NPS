3614abaded67d99ff5879bc1123d06a0ff1b9493
YuebanZhou <598065612@qq.com>
2018-06-10 18:25:47 +0800

    201806101825

---
+++ mapdemo2/static/js/appThree.js
@@ -0,0 +1,362 @@
+//绘制index页面上静态的折线图
+var lineChart1 = echarts.init(document.getElementById("echarts-line-chart1"));
+var icons = {
+  yidong: "../img/yidong.png",
+  dianxin: "../img/dianxin.png",
+  liantong: "../img/liantong.png"
+};
+var lineoption1 = {
+  title: {
+    // text: '年度完成率：68%\n年度提升值：5.4\n季度提升值：5.4',
+    textStyle: {
+      color: "#fff",
+      fontSize: 14
+    }
+  },
+  tooltip: {
+    trigger: "axis"
+  },
+  grid: {
+    left: "3%",
+    right: "10%",
+    bottom: "3%",
+    containLabel: true
+  },
+  xAxis: {
+    type: "category",
+    boundaryGap: false,
+    data: ["17Q1", "17Q2", "17Q3", "17Q4", "18Q1"],
+    axisLabel: {
+      show: true,
+      textStyle: {
+        color: "#fff"
+      },
+      //数据全部展示，不隐藏
+      interval: 0
+    },
+    axisLine: {
+      // show:true,
+      lineStyle: {
+        color: "#333"
+      }
+    },
+    splitLine: {
+      lineStyle: {
+        color: "#333"
+      }
+    }
+  },
+  yAxis: {
+    show: true,
+    type: "value",
+    data: ["yidong", "dianxin", "liantong"],
+    axisLabel: {
+      show: true,
+      textStyle: {
+        color: "#fff"
+      },
+      rich: {
+        yidong: {
+          backgroundColor: {
+            image: icons.yidong
+          }
+        },
+        dainxin: {
+          backgroundColor: {
+            image: icons.dainxin
+          }
+        },
+        liantong: {
+          backgroundColor: {
+            image: icons.liantong
+          }
+        }
+      }
+      //数据全部展示，不隐藏
+      //interval: 0
+    },
+    axisLine: {
+      show: true,
+      lineStyle: {
+        color: "#333"
+      }
+    },
+    splitLine: {
+      lineStyle: {
+        color: "#333"
+      }
+    }
+  },
+  series: [
+    {
+      name: "中国联通",
+      type: "line",
+      // stack: '总量',
+      data: [
+        -1.2,
+        -0.5,
+        0.6,
+        5,
+        {
+          value: 5,
+          label: {
+            normal: {
+              textStyle: {
+                fontSize: 30
+              }
+            }
+          }
+        }
+      ],
+      symbolSize: 6,
+      itemStyle: {
+        normal: {
+          color: "#D2000B",
+          label: {
+            show: true
+          }
+        }
+      }
+    },
+    {
+      name: "中国移动",
+      type: "line",
+      // stack: '总量',
+      data: [
+        28.4,
+        23.4,
+        24.2,
+        26.9,
+        {
+          value: 23.8,
+          label: {
+            normal: {
+              textStyle: {
+                fontSize: 30
+              }
+            }
+          }
+        }
+      ],
+      symbolSize: 6,
+      itemStyle: {
+        normal: {
+          color: "#95C72D",
+          label: {
+            show: true
+          }
+        }
+      }
+    },
+    {
+      name: "中国电信",
+      type: "line",
+      // stack: '总量',
+      data: [
+        14.1,
+        13.9,
+        14.4,
+        17.9,
+        {
+          value: 16.5,
+          label: {
+            normal: {
+              textStyle: {
+                fontSize: 30
+              }
+            }
+          }
+        }
+      ],
+      symbolSize: 6,
+      itemStyle: {
+        normal: {
+          color: "#053F9D",
+          label: {
+            show: true
+          }
+        }
+      }
+    }
+  ]
+};
+lineChart1.setOption(lineoption1);
+
+//绘制index页面上静态的折线图
+var lineChart2 = echarts.init(document.getElementById("echarts-line-chart2"));
+var lineoption2 = {
+  title: {
+    // text: '年度完成率： \n年度提升值： \n季度提升值： ',
+    textStyle: {
+      color: "#fff",
+      fontSize: 14
+    }
+  },
+  tooltip: {
+    trigger: "axis"
+    // position: [390, 10]
+  },
+  legend: {
+    data: ["中国联通", "中国移动", "中国电信"],
+    textStyle: {
+      color: "#fff",
+      fontSize: 12
+    }
+  },
+  grid: {
+    left: "3%",
+    right: "10%",
+    bottom: "3%",
+    containLabel: true
+  },
+  xAxis: {
+    type: "category",
+    boundaryGap: false,
+    data: ["17Q1", "17Q2", "17Q3", "17Q4", "18Q1"],
+    axisLabel: {
+      show: true,
+      textStyle: {
+        color: "#fff"
+      },
+      //数据全部展示，不隐藏
+      interval: 0
+    },
+    axisLine: {
+      // show:true,
+      lineStyle: {
+        color: "#333"
+      }
+    },
+    splitLine: {
+      lineStyle: {
+        color: "#333"
+      }
+    }
+  },
+  yAxis: {
+    show: true,
+    type: "value",
+    axisLabel: {
+      show: true,
+      textStyle: {
+        color: "#fff"
+      },
+      //数据全部展示，不隐藏
+      interval: 0
+    },
+    axisLine: {
+      // show:true,
+      lineStyle: {
+        color: "#333"
+      }
+    },
+    splitLine: {
+      lineStyle: {
+        color: "#333"
+      }
+    }
+  },
+  series: [
+    {
+      name: "中国联通",
+      type: "line",
+      // stack: '总量',
+      data: [
+        -0.1,
+        -1.1,
+        0.8,
+        4.2,
+        {
+          value: 7.5,
+          label: {
+            normal: {
+              textStyle: {
+                fontSize: 30
+              }
+            }
+          }
+        }
+      ],
+      symbolSize: 6,
+      itemStyle: {
+        normal: {
+          color: "#D2000B",
+          label: {
+            show: true
+          }
+        }
+      }
+    },
+    {
+      name: "中国移动",
+      type: "line",
+      // stack: '总量',
+      data: [
+        ,
+        ,
+        ,
+        ,
+        {
+          value: -0.7,
+          label: {
+            normal: {
+              textStyle: {
+                fontSize: 30
+              }
+            }
+          }
+        }
+      ],
+      symbolSize: 6,
+      itemStyle: {
+        normal: {
+          color: "#95C72D",
+          label: {
+            show: true
+          }
+        }
+      }
+    },
+    {
+      name: "中国电信",
+      type: "line",
+      // stack: '总量',
+      data: [
+        ,
+        ,
+        ,
+        ,
+        {
+          value: 6.9,
+          label: {
+            normal: {
+              textStyle: {
+                fontSize: 30
+              }
+            }
+          }
+        }
+      ],
+      symbolSize: 6,
+      itemStyle: {
+        normal: {
+          color: "#053F9D",
+          label: {
+            show: true
+          }
+        }
+      }
+    }
+  ]
+};
+
+lineChart2.setOption(lineoption2);
+
+$(".ibox3").click(function() {
+  console.log("ibox3");
+  window.location.href = "indexx.html";
+});
+$(".ibox4").click(function() {
+  console.log("ibox4");
+  window.location.href = "indexx.html";
+});
---
+++ mapdemo2/static/js/table.js
@@ -0,0 +1,34 @@
+var str=''
+for(var i=0;i<31;i++){
+    str+=`<tr>
+    <td width="75" align="center">1</td>
+    <td width="75" align="center">江苏</td>
+    <td width="75" align="center">100%</td>
+    <td width="75" align="center">11.11</td>
+    <td width="75" align="center">10.69</td>
+    <td width="75" align="center">1</td>
+  </tr>`
+}
+$("#tplWrapper_TemplateZ").html(str)
+var tr=$("#tplWrapper_TemplateZ").find("tr")
+var imgstr1="<img src='static/img/jbb1.png' style='height:23px;width:23px;'>"
+var imgstr2="<img src='static/img/jbb2.png' style='height:23px;width:23px;'>"
+var imgstr3="<img src='static/img/jbb3.png' style='height:23px;width:23px;'>"
+console.log(tr)
+for(var i=0;i<tr.length;i++){
+  var td = tr.eq(i).find("td");  
+  var num = td.eq(0).text();
+  console.log(num)
+  if(num=="1"){
+    console.log(i)
+    tr.eq(i).find("td").eq(0).html(imgstr1)
+  }
+  if(num=="2"){
+    console.log(i)
+    tr.eq(i).find("td").eq(0).html(imgstr2)
+  }
+  if(num=="3"){
+    console.log(i)
+    tr.eq(i).find("td").eq(0).html(imgstr3)
+  }
+}
